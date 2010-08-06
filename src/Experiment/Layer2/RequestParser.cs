9da3d6800c3225d70772bc8b3dcd46ca720b6223
hammett <hammett@gmail.com>
2010-08-06 00:47:52 -0700

    Almost working

---
+++ src/Experiment/Layer2/RequestParser.cs
@@ -0,0 +1,15 @@
+﻿namespace Layer2
+{
+    using System.ComponentModel.Composition;
+    using System.Web;
+    using System.Web.Routing;
+
+    [Export]
+    public class RequestParser
+    {
+        public RouteData ParseDescriminators(HttpRequestBase request)
+        {
+            return request.RequestContext.RouteData;
+        }
+    }
+}
